aeac1def6dfc23a28fb4c263e426829ae89ea7ed
andreatoson3-netizen <andrea.toson.3@studenti.unipd.it>
2026-08-29 00:25:28 +0200

    Update CityObserver.java

--- src/main/java/com/citylogic/simulation/CityObserver.java
+++ src/main/java/com/citylogic/simulation/CityObserver.java
@@ -1,7 +1,10 @@
 package com.citylogic.simulation;
 
 import com.citylogic.model.Stats;
-
+//interfaccia per il pattern Observer
+//permette agli elementi in ascolto di 
+//ricevere aggiornamenti automatici sullo stato delle città ogni volta che si verificano cambiamenti
 public interface CityObserver {
+    //metodo usato per notificare i cambiamenti di stato
     void update(Stats currentStats);
 }
