97d89f3a95b57fd2959a23c23defdc482fbed517
Sere <serena.barbisan@studenti.unipd.it>
2026-08-28 21:52:07 +0200

    Update Grid.java

--- src/main/java/com/citylogic/model/Grid.java
+++ src/main/java/com/citylogic/model/Grid.java
@@ -86,6 +86,10 @@ public class Grid {
         return count;
     }
 
+    public int getBlackoutCount() {
+        return blackoutQueue.size();
+    }
+
 
 
     // Accende e spegne le celle in base alla disponibilità di corrente e gestisce la coda di blackout
