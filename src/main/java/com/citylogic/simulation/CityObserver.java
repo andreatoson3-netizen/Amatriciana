05d69ce68525df8ad2a09634fcce42b125554034
Sere <serena.barbisan@studenti.unipd.it>
2026-08-29 04:48:06 +0200

    Update CityObserver.java

--- src/main/java/com/citylogic/simulation/CityObserver.java
+++ src/main/java/com/citylogic/simulation/CityObserver.java
@@ -1,10 +1,12 @@
 package com.citylogic.simulation;
 
 import com.citylogic.model.Stats;
-//interfaccia per il pattern Observer
-//permette agli elementi in ascolto di 
-//ricevere aggiornamenti automatici sullo stato delle città ogni volta che si verificano cambiamenti
+
+// Interfaccia utilizzata per il pattern Observer.
+// Permette agli oggetti che osservano la città di ricevere
+// aggiornamenti quando cambiano le statistiche della simulazione
 public interface CityObserver {
-    //metodo usato per notificare i cambiamenti di stato
+    
+    // Notifica l'Observer fornendo le statistiche aggiornate della città
     void update(Stats currentStats);
 }
