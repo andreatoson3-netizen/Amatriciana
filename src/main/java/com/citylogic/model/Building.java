f302d5864e32449aae559072ca7b8705cc64fe92
Sere <serena.barbisan@studenti.unipd.it>
2026-08-29 04:12:31 +0200

    Update Building.java

--- src/main/java/com/citylogic/model/Building.java
+++ src/main/java/com/citylogic/model/Building.java
@@ -1,22 +1,23 @@
 package com.citylogic.model;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-//classe astratta che rappresenta un edificio generico all'interno della città
-//estende Cell ed è specializzata in edifici residenziali, industriali e commerciali
+// Classe astratta che rappresenta un edificio generico all'interno della città.
+// È specializzata nelle tipologie Residential, Factory e Commercial.
 
-//scritture per Jackson
+// Configurazione necessaria a Jackson per riconoscere il tipo concreto
+// dell'edificio durante la persistenza JSON.
 @JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY,property="@class")
 
 
 public abstract class Building extends Cell {
-    //costruttore vuoto di default necessario per Jackson(persistenza JSON)
+    //Costruttore vuoto di default necessario per Jackson(persistenza JSON)
     public Building(){
         super();
-        this.free=false;//edificio occupa il blocco, quindi non è piu' libero
+        this.free=false; // Un edificio occupa la cella, quindi non è libera.
     }
 
-    //metodo astratto ereditato da Block che ciascun edificio concreto
-    // (Residential,Factory,Commercial) dovrà implementare per restiruire le proprie metriche
+    // Ogni edificio concreto implementa questo metodo per restituire
+    // gli effetti sulle metriche della città.
     @Override
     public abstract Stats returnStat();
 
